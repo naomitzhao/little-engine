2e5406bb30088db664c46c90d23b6e27d79fd1b1
Naomi Zhao <naomitan.zhao@gmail.com>
2026-02-10 16:11:15 -0800

    Merge test

--- src/entity.cpp
+++ src/entity.cpp
@@ -3,17 +3,17 @@
 Entity::Entity(float w, float h): id(-1), x(0), y(0), vx(0), vy(0), ax(0), ay(0), width(w), height(h), grounded(false) {}
 
 void Entity::updateX(float deltaTime) {
-    x += vx * deltaTime;
-}
-
-void Entity::updateY(float deltaTime) {
-    y += vy * deltaTime;
+    x += vx * deltaTime * 1;
 }
 
 void Entity::applyGravity(float gravity, float deltaTime) {
     if (!grounded) {
         vy += gravity * deltaTime;
     }
+}
+
+void Entity::updateY(float deltaTime) {
+    y += vy * deltaTime;
 }
 
 void Entity::jump(float jumpStrength) {
